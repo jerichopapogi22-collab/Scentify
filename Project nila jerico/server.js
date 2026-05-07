f4aa62ef26acdab336a5c303713c9e05299c188c
jerichopapogi22 <jerichopapogi22@gmail.com>
2026-05-08 07:53:32 +0800

    gmail step

--- Project nila jerico/server.js
+++ Project nila jerico/server.js
@@ -16,12 +16,14 @@
     📧 GMAIL SMTP EMAIL SETUP
     ===================================================== */
     const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
     transporter.verify((error) => {
         if (error) {
