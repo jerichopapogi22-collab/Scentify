253873c34335947edc80a1fbea48c51ad932924f
jerichopapogi22 <jerichopapogi22@gmail.com>
2026-05-08 12:03:20 +0800

    fix mongo connection + resend setup

--- Project nila jerico/server.js
+++ Project nila jerico/server.js
@@ -1,4 +1,10 @@
-﻿require("dotenv").config();
+﻿const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log("❌ MongoDB error:", err));
+  
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
